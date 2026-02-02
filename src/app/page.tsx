d7444130b1db5fc4dc7f0328e1126ce4640c073e
ducnm168 <ductiny2003@gmail.com>
2026-02-03 05:27:27 +0700

    working on login and signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AuthScreen from "@/components/home/auth-screen/AuthScreen";
 import HomeScreen from "@/components/home/home-screen/HomeScreen";
 
 export default function Home() {
-  const user = true;
+  const user = false
   return (
     <main>
       {user ? <HomeScreen /> : <AuthScreen />}
